de1a8962474f60f6097867b9b77bb46cf13b36e7
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-11-02 18:45:26 -0400

    scratch docker

--- cmd/gog/main_test.go
+++ cmd/gog/main_test.go
@@ -36,6 +36,7 @@ func TestRoot(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/", nil)
 	response := executeRequest(req)
 
+
 	checkResponseCode(t, http.StatusOK, response.Code)
 
 	if body := response.Body.String(); body !=
